dcb4734c12ec9e64b891dac978a29b1ff274c624
S Romakin <romakinsv@local>
2020-09-16 22:15:19 +0300

    Changes to simplify

--- src/main/java/Task1/PersonBuilder.java
+++ src/main/java/Task1/PersonBuilder.java
@@ -1,73 +1,46 @@
 package Task1;
 
-import java.util.HashMap;
-
 public class PersonBuilder {
 
-    private Person person;
-    private HashMap<String, String> personAttrs;
-    private int bufAge = -1;
-
-    public PersonBuilder() {
-        this.person = null;
-        this.personAttrs = new HashMap<>();
-    }
+    private String personName;
+    private String personSurname;
+    private String personAddress;
+    private int personAge = -1;
 
     public PersonBuilder setName(String name) {
-        personAttrs.put("name", name);
+        personName = name;
         return this;
     }
+
     public PersonBuilder setSurname(String surname) {
-        personAttrs.put("surname", surname);
+        personSurname = surname;
         return this;
     }
+    
     public PersonBuilder setAge(int age) {
         if (age < 0)
             throw new IllegalArgumentException("Age must be more than 0");
-        if (person != null && !person.hasAge()) {
-            person.setAge(age);
-        } else {
-            bufAge = age;
-            renderPerson(false);
-        }
+        personAge = age;
         return this;
     }
+
     public PersonBuilder setAddress(String address) {
         if (address == null || "".equals(address.trim()))
             throw new IllegalArgumentException("Address can not be empty or null");
-        if (person != null && !person.hasAddress()) {
-            person.setAddress(address);
-        } else {
-            personAttrs.put("address", address);
-            renderPerson(false);
-        }
+        personAddress = address;
         return this;
     }
 
-    private void renderPerson(boolean finalRender) {
-        if (person == null &&
-                personAttrs.containsKey("name")&&
-                personAttrs.containsKey("surname")) {
-            if (bufAge < 0) {
-                person = new Person(
-                        personAttrs.get("name"),
-                        personAttrs.get("surname")
-                );
-            } else {
-                person =  new Person(
-                        personAttrs.get("name"),
-                        personAttrs.get("surname"),
-                        bufAge
-                );
-            }
-            if (personAttrs.containsKey("address")) {
-                person.setAddress(personAttrs.get("address"));
-            }
-        } else if (finalRender) throw new IllegalStateException("Build failed: name and surname is null");
-    }
-
     public Person build() {
-        if (person == null) renderPerson(true);
-        return person;
+        if (personName != null && personSurname != null)
+            throw new IllegalStateException("Build failed: name and surname is null");
+        Person result;
+        if (personAge > 0) {
+            result = new Person(personName, personSurname, personAge);
+        } else {
+            result =  new Person(personName, personSurname);
+        }
+        if (personAddress != null) result.setAddress(personAddress);
+        return result;
     }
 }
